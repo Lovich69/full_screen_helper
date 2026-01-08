d8910efc9b49da89041e8430c6f0681d85baf975
Lorenzo Pierluigi Vassallo <lorenzo@Lorenzos-MacBook-Pro.local>
2026-01-08 15:18:25 -0300

    Initial commit

---
+++ full_screen_helper/test/full_screen_helper_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:full_screen_helper/full_screen_helper.dart';
+
+void main() {
+  test('FullScreenHelper class exists', () {
+    expect(FullScreenHelper, isNotNull);
+  });
+}
